28883bfb8d631e6861e01c22f8ae04790c8f0d45
Bernardo Conejo <quinasa_05_radian@icloud.com>
2025-02-17 18:46:06 +0100

    feat: Añadir archivo de encabezado con estructura básica HTML

---
+++ header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <title>Sistema CRUD de inventarios de productos</title>
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="icon" type="image/x-icon" href="favicon.png" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    </head>
+
+    <body>
+        <header class="container-fluid bg-warning text-center py-3 display-4">
+            Sistema Gestor de Inventariado de productos
+        </header>
+        <main>
